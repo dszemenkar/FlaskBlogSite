896342153fce168c0951bc3bb3c223de07edee96
David Szemenkar <dszemenkar@icloud.com>
2020-10-13 16:21:50 +0200

    Added error handling and core components.

--- blogsite/__init__.py
+++ blogsite/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from blogsite.core.views import core
+from blogsite.error_pages.handlers import error_pages
+
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
